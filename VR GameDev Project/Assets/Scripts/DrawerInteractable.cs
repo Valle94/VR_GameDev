d3e8e11b4541d95b474618efdea2113aff8565cb
Max Valle <55932182+Valle94@users.noreply.github.com>
2026-02-12 14:38:11 -0500

    3.5 Customizing Drawer Grab Interactions
    Implemented code-based drawer interactions using layer masks.

--- VR GameDev Project/Assets/Scripts/DrawerInteractable.cs
+++ VR GameDev Project/Assets/Scripts/DrawerInteractable.cs
@@ -1,12 +1,17 @@
 using System;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
-public class DrawerInteractable : MonoBehaviour
+public class DrawerInteractable : XRGrabInteractable
 {
     [SerializeField] XRSocketInteractor keySocket;
     [SerializeField] bool isLocked;
+
+    private Transform parentTransform;
+    private const string defaultLayer = "Default";
+    private const string grabLayer = "Grab";
 
     void Start()
     {
@@ -15,6 +20,8 @@ public class DrawerInteractable : MonoBehaviour
             keySocket.selectEntered.AddListener(OnDrawerUnlocked);
             keySocket.selectExited.AddListener(OnDrawerLocked);
         }
+
+        parentTransform = transform.parent.transform;
     }
 
     private void OnDrawerUnlocked(SelectEnterEventArgs arg0)
@@ -29,6 +36,24 @@ public class DrawerInteractable : MonoBehaviour
         Debug.Log("***DRAWER LOCKED");
     }
 
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+        if(!isLocked)
+        {
+            transform.SetParent(parentTransform);
+        }
+        else
+        {
+            interactionLayers = InteractionLayerMask.GetMask(defaultLayer);
+        }
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        base.OnSelectExited(args);
+        interactionLayers = InteractionLayerMask.GetMask(grabLayer);
+    }
     void Update()
     {
         
