4be8b868fd1ebcd09a686404860856c9f3e7947e
Max Valle <55932182+Valle94@users.noreply.github.com>
2026-02-23 04:22:32 -0500

    4.5 Changing the Message Text with Code
    Adjusted the message text to change when the UI button is pressed.

---
+++ VR GameDev Project/Assets/Scripts/SimpleUIControl.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.XR.Interaction.Toolkit;
+using System;
+
+public class SimpleUIControl : MonoBehaviour
+{
+    [SerializeField] XRButtonInteractable startButton;
+    [SerializeField] string[] msgStrings;
+    [SerializeField] TMP_Text[] msgTexts;
+    [SerializeField] GameObject keyIndicatorLight;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if(startButton != null)
+        {
+            startButton.selectEntered.AddListener(StartButtonPressed);
+        }
+    }
+
+    private void StartButtonPressed(SelectEnterEventArgs arg0)
+    {
+        SetText(msgStrings[1]);
+        if(keyIndicatorLight != null)
+        {
+            keyIndicatorLight.SetActive(true);            
+        }
+    }
+
+    public void SetText(string msg)
+    {
+        for (int i = 0; i < msgTexts.Length; i++)
+        {
+            msgTexts[i].text = msg;
+        }
+    }
+}
