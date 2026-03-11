3a6371bd256d59780280b3bdaaa8f1c7f3d73409
Max Valle <55932182+Valle94@users.noreply.github.com>
2026-03-11 12:41:18 -0400

    7.3 Adding Grab Interactable SFX
    Implemented grab interactable sfx for cube, wand, and key.

--- VR GameDev Project/Assets/Scripts/System/XRAudioManager.cs
+++ VR GameDev Project/Assets/Scripts/System/XRAudioManager.cs
@@ -1,16 +1,33 @@
 using System;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class XRAudioManager : MonoBehaviour
 {
+    [SerializeField] XRGrabInteractable[] grabInteractables;
+    [SerializeField] AudioSource grabSound;
+    [SerializeField] AudioClip grabClip;
+    [SerializeField] AudioClip keyclip;
+    [SerializeField] AudioSource activatedSound;
+    [SerializeField] AudioClip grabActivatedClip;
+    [SerializeField] AudioClip wandActivatedClip;
     [SerializeField] TheWall wall;
     [SerializeField] AudioSource wallSource;
     [SerializeField] AudioClip destroyWallClip;
     [SerializeField] private AudioClip fallbackClip;
     private const string FallBackClip_Name = "fallbackClip";
 
-     void OnEnable()
+    void OnEnable()
     {
+        grabInteractables = FindObjectsByType<XRGrabInteractable>(FindObjectsSortMode.None);
+        for (int i = 0; i < grabInteractables.Length; i++)
+        {
+            grabInteractables[i].selectEntered.AddListener(OnSelectEnterGrabbable);
+            grabInteractables[i].selectExited.AddListener(OnSelectExitGrabbable);
+            grabInteractables[i].activated.AddListener(OnActivatedGrabbable);
+
+        }
         if (fallbackClip != null)
         {
             fallbackClip = AudioClip.Create(FallBackClip_Name, 1, 1, 1000, true);
@@ -25,6 +42,39 @@ public class XRAudioManager : MonoBehaviour
             }
             wall.OnDestroy.AddListener(OnDestroyWall);
         }
+    }
+
+    private void OnActivatedGrabbable(ActivateEventArgs arg0)
+    {
+        GameObject tempGameObject = arg0.interactableObject.transform.gameObject;
+        if (tempGameObject.GetComponent<WandControl>() != null)
+        {
+            activatedSound.clip = wandActivatedClip;
+        }
+        else
+        {
+            activatedSound.clip = grabActivatedClip;
+        }
+        activatedSound.Play();
+    }
+
+    private void OnSelectExitGrabbable(SelectExitEventArgs arg0)
+    {
+        grabSound.clip = grabClip;
+        grabSound.Play();
+    }
+
+    private void OnSelectEnterGrabbable(SelectEnterEventArgs arg0)
+    {
+        if (arg0.interactableObject.transform.CompareTag("Key"))
+        {
+            grabSound.clip = keyclip;
+        }
+        else
+        {
+            grabSound.clip = grabClip;
+        }
+        grabSound.Play();
     }
 
     private void OnDestroyWall()
